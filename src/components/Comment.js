fd9237fbebd1093201b888fbe4e8165dc6ef49ac
Shea Cronin <shealcronin@gmail.com>
2022-04-11 12:03:04 -0400

    Format dates for posts and comments

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,11 +1,17 @@
+import { formatRelative, parseISO } from 'date-fns';
 import '../styles/Comment.css';
 
 function Comment({ comment }) {
     return (
         <div className="comment">
-            <h4 className="comment-author">
-                {comment.author.displayName} says...
-            </h4>
+            <div className="comment-info">
+                <h4 className="comment-author">
+                    {comment.author.displayName} says...
+                </h4>
+                <p className="comment-timestamp">
+                    {formatRelative(parseISO(comment.timestamp), new Date())}
+                </p>
+            </div>
             <p>{comment.content}</p>
         </div>
     );
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,15 +1,20 @@
 import '../styles/Post.css';
 import CommentsSection from './CommentsSection';
+import { formatRelative, parseISO } from 'date-fns';
 
 function Post({ post, user }) {
     return (
         <article className="post">
-            <h2>{post.title}</h2>
+            <div className="post-info">
+                <h2>{post.title}</h2>
+                <p className="post-timestamp">
+                    {formatRelative(parseISO(post.timestamp), new Date())}
+                </p>
+            </div>
             <p>
                 {post.author.displayName} •{' '}
-                <span className="display-name">@{post.author.username}</span>
+                <span className="username">@{post.author.username}</span>
             </p>
-            <p>{post.timestamp}</p>
             <p>{post.content}</p>
             <CommentsSection post={post} user={user} />
         </article>
