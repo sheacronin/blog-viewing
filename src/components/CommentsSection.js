b826e0596a3a2e196b9118a707c9a705e75f21f5
Shea Cronin <shealcronin@gmail.com>
2022-04-11 11:31:07 -0400

    Break out CommentsSection into its own component and allow comments to be hidden/expanded

---
+++ src/components/CommentsSection.js
@@ -0,0 +1,73 @@
+import Comment from './Comment';
+import { useState } from 'react';
+
+function CommentsSection({ post, user }) {
+    const [comments, setComments] = useState(post.comments);
+    const [areCommentsExpanded, setAreCommentsExpanded] = useState(false);
+
+    function handleCommentFormSubmission(e) {
+        e.preventDefault();
+        const { content } = e.target.elements;
+
+        postNewComment().then((data) => {
+            setComments((prevComments) => [...prevComments, data.comment]);
+            content.value = '';
+        });
+
+        async function postNewComment() {
+            const res = await fetch(
+                `http://localhost:3001/posts/${post._id}/comments`,
+                {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        content: content.value,
+                    }),
+                    credentials: 'include',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization:
+                            'Bearer ' + localStorage.getItem('token'),
+                    },
+                }
+            );
+            const data = await res.json();
+            return data;
+        }
+    }
+
+    function handleExpandComments() {
+        setAreCommentsExpanded((prevState) => !prevState);
+    }
+
+    return (
+        <section className="comments">
+            <h3>Comments</h3>
+            {comments.map((comment, index) => {
+                if (!areCommentsExpanded && index > 1) {
+                    return null;
+                }
+                return <Comment key={comment._id} comment={comment} />;
+            })}
+            {comments.length > 2 && (
+                <button
+                    className="expand-comments"
+                    onClick={handleExpandComments}
+                >
+                    {areCommentsExpanded ? 'Hide' : 'Expand'} Comments
+                </button>
+            )}
+            {user !== null && (
+                <form
+                    className="comment-form"
+                    onSubmit={handleCommentFormSubmission}
+                >
+                    <label htmlFor="content">Your New Comment:</label>
+                    <textarea type="text" id="content" name="content" />
+                    <button type="submit">Post Comment</button>
+                </form>
+            )}
+        </section>
+    );
+}
+
+export default CommentsSection;
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,40 +1,7 @@
-import { useState } from 'react';
 import '../styles/Post.css';
-import Comment from './Comment';
+import CommentsSection from './CommentsSection';
 
 function Post({ post, user }) {
-    const [comments, setComments] = useState(post.comments);
-
-    function handleCommentFormSubmission(e) {
-        e.preventDefault();
-        const { content } = e.target.elements;
-
-        postNewComment().then((data) => {
-            setComments((prevComments) => [...prevComments, data.comment]);
-            content.value = '';
-        });
-
-        async function postNewComment() {
-            const res = await fetch(
-                `http://localhost:3001/posts/${post._id}/comments`,
-                {
-                    method: 'POST',
-                    body: JSON.stringify({
-                        content: content.value,
-                    }),
-                    credentials: 'include',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        Authorization:
-                            'Bearer ' + localStorage.getItem('token'),
-                    },
-                }
-            );
-            const data = await res.json();
-            return data;
-        }
-    }
-
     return (
         <article className="post">
             <h2>{post.title}</h2>
@@ -44,22 +11,7 @@ function Post({ post, user }) {
             </p>
             <p>{post.timestamp}</p>
             <p>{post.content}</p>
-            <section className="comments">
-                <h3>Comments</h3>
-                {comments.map((comment) => (
-                    <Comment key={comment._id} comment={comment} />
-                ))}
-                {user !== null && (
-                    <form
-                        className="comment-form"
-                        onSubmit={handleCommentFormSubmission}
-                    >
-                        <label htmlFor="content">Your New Comment:</label>
-                        <textarea type="text" id="content" name="content" />
-                        <button type="submit">Post Comment</button>
-                    </form>
-                )}
-            </section>
+            <CommentsSection post={post} user={user} />
         </article>
     );
 }
